2aa383d1875fb6a1e19531235cbbf0f7222ca894
Nour El Houda Ouni <100380823+nourouni@users.noreply.github.com>
2025-05-12 17:43:15 +0100

    removed api soil

--- crop-disease-image-classification-web-app-main/soil_composition/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('soil-lookup/', views.soil_lookup, name='soil_lookup'),
-    path('api/get-location/', views.get_location, name='get_location'),
-    path('api/search-by-city/', views.search_by_city, name='search_by_city'),
-    path('api/get-soil-data/', views.get_soil_data_ajax, name='get_soil_data_ajax'),
-]
