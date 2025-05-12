b9d7674f94d1fcd0b49a778d501a4620ebe8ced1
Aziz7905 <114614709+Aziz7905@users.noreply.github.com>
2025-05-12 12:23:04 +0200

    LLM&VisualUpdates

--- crop-disease-image-classification-web-app-main/crop_disease/settings.py
+++ crop-disease-image-classification-web-app-main/crop_disease/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -159,3 +160,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+load_dotenv()
--- crop-disease-image-classification-web-app-main/crop_rotation_recommendation/views.py
+++ crop-disease-image-classification-web-app-main/crop_rotation_recommendation/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .utils.qa_chain import answer_question
+import json
 
-# Create your views here.
+@csrf_exempt
+def chat_view(request):
+    if request.method == "GET":
+        return render(request, "crop_rotation_recommendation/chat.html")
+
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            question = data.get("question", "").strip()
+            if not question:
+                return JsonResponse({"response": "No question provided."}, status=400)
+
+            answer = answer_question(question)
+            return JsonResponse({"response": answer})
+        except json.JSONDecodeError as e:
+            return JsonResponse({"response": f"Invalid JSON data: {str(e)}"}, status=400)
+        except Exception as e:
+            return JsonResponse({"response": f"Server error: {str(e)}"}, status=500)
+        
+
+    return JsonResponse({"response": "Only GET and POST allowed"}, status=405)
